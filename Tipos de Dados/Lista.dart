48c3302c558370695db13e68fb8fcd720d8faf7c
belmiro-kunga <belmirokunga@gmail.com>
2021-09-25 02:23:33 +0100

    Listas

---
+++ Tipos de Dados/Lista.dart
@@ -0,0 +1,43 @@
+//Lista Fixa
+
+class frutas {
+  String nome;
+  int quantidade;
+
+  frutas(this.nome, this.quantidade);
+}
+
+void main() {
+  //List<int> numero = [1,2,3,4,5];
+
+  //print(numero.length); //Tamanho da Lista
+
+  // print(numero[4]); //Imprimir o elemento no index 4
+
+  //List<String> frutas = ['Banana', 'Ananas', 'Abacate'];
+
+  //print(frutas);
+
+  //frutas.add('Manga'); // Adicionar um elemento na lista
+
+  //print(frutas);
+
+  // frutas.removeAt(1); // Remover um elemento da lista
+  //print(frutas);
+
+  //frutas.insert(2, 'Limão'); // inserção de elemento da Lista
+  //print(frutas);
+
+//  print(frutas.contains('Pão')); //checar alista para ver se existe um determinado elemento
+
+  List<frutas> listFruta = List();
+
+  listFruta.add(frutas('Banana', 10));
+  listFruta.add(frutas('Manga', 20));
+
+  for (frutas f in listFruta) {
+    print(f.nome);
+  }
+
+  frutas.
+}
--- Tipos de Dados/Lista_tridimensional.dart
+++
@@ -1,7 +0,0 @@
-//Lista tridimensional
-void main() {
-  var gfg = List.generate(
-      3, (i) => List.generate(3, (j) => List.generate(3, (k) => i + j + k)));
-
-  print(gfg);
-}
